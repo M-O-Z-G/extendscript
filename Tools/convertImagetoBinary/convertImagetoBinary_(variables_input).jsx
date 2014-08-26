490120f1eacc895c0140377711f034b3562bea18
Aleksandr M.O.Z.G Dikov <mozg@mozg-studio.org>
2014-08-26 06:36:27 +0400

    +

---
+++ Tools/convertImagetoBinary/convertImagetoBinary_(variables_input).jsx
@@ -0,0 +1,7 @@
+﻿var fileInput = File ("/d/test_jpg/icon.png");
+var fileOutput = File ("/d/test_jpg/icon.txt");
+fileInput.open ("r"); fileInput.encoding = "binary";
+var temp = fileInput.read(); fileInput.close();
+fileOutput.open ("w");
+fileOutput.write (temp.toSource ());
+fileOutput.close ();
